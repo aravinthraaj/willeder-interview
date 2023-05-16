17b5602e5830a14e60185bcef9bd546cf1e8ba7b
aravinthraajk <aravinthraajk@gmail.com>
2023-05-16 15:12:08 +0530

    refactor: home page added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from "react"
 import "./styles/global.scss"
 import Navbar from "./components/templates/Navbar/Navbar"
-import Banner from "./components/templates/Banner/Banner"
-import HomeNavbar from "./components/templates/HomeNavbar/HomeNavbar"
-import Footer from "./components/templates/Footer/Footer"
-import Motto from "./components/templates/Motto/Motto"
-import Strengths from "./components/templates/Strengths/Strengths"
-import WhoWeAre from "./components/templates/WhoWeAre/WhoWeAre"
-import Notice from "./components/templates/Notice/Notice"
-import { sections } from "./constants/constants"
+import Home from "./pages/Home/Home"
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <Banner />
-      <HomeNavbar sections={sections} />
-      <Motto sectionId={"MPOとは"} />
-      <Strengths sectionId={"MPOの強み"} />
-      <WhoWeAre sectionId={"MPOとTLOとの違い"} />
-      <Notice sectionId={"お知らせ"} />
-      <Footer />
+      <Home />
     </div>
   )
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import Banner from "../../components/templates/Banner/Banner"
+import HomeNavbar from "../../components/templates/HomeNavbar/HomeNavbar"
+import Footer from "../../components/templates/Footer/Footer"
+import Motto from "../../components/templates/Motto/Motto"
+import Strengths from "../../components/templates/Strengths/Strengths"
+import WhoWeAre from "../../components/templates/WhoWeAre/WhoWeAre"
+import Notice from "../../components/templates/Notice/Notice"
+import { sections } from "../../constants/constants"
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <HomeNavbar sections={sections} />
+      <Motto sectionId={"MPOとは"} />
+      <Strengths sectionId={"MPOの強み"} />
+      <WhoWeAre sectionId={"MPOとTLOとの違い"} />
+      <Notice sectionId={"お知らせ"} />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
